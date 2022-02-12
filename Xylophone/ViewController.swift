63e238c4de78aa4b912cef669745953cb9e31389
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-02-12 09:12:15 -0300

    Implementation of Timer for change the button's opacity

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,13 +11,25 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         let key = sender.currentTitle!
-
         playSound(with: key)
+        print("Start")
+        sender.layer.opacity = 0.5
+        startTimer(sender)
     }
     
     func playSound(with key: String) {
         let url = Bundle.main.url(forResource: key, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
+    }
+    
+    func startTimer(_ sender: UIButton) {
+        _ = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(changeOpacity), userInfo: sender, repeats: false)
+    }
+                                         
+    @objc func changeOpacity(_ timer: Timer) {
+        let button = timer.userInfo as? UIButton
+        button?.layer.opacity =  1
+        print("End")
     }
 }
